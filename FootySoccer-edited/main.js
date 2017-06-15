8117f7b71e11ede1a8bdcb67fac9e82a0b226927
onyce <Killbil7>
2017-06-15 10:04:06 -0700

    Remove useless dumb thing

--- FootySoccer-edited/main.js
+++
@@ -1,9 +0,0 @@
-var game = new Phaser.Game(1400, 600, Phaser.AUTO, '');
-
-game.state.add("game", gameState);
-game.state.add("menu", menuState);
-game.state.add("winblue", winStateBlue);
-game.state.add("winred", winStateRed);
-game.state.add("instructions", instructState);
-
-game.state.start("menu");
